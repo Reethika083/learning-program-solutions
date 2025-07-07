d5c3b6106b469ffbfe920119a2c1a72c15dcb412
Reethika <itsme.reethu004@gmail.com>
2025-07-07 21:53:19 +0530

    Ist ex

--- Week3/Week3_SpringCore_Maven_HandsOn/09_SpringBootApp/src/main/java/com/library/repository/BookRepository.java
+++ Week3/Week3_SpringCore_Maven_HandsOn/09_SpringBootApp/src/main/java/com/library/repository/BookRepository.java
@@ -3,5 +3,4 @@ package com.library.repository;
 import com.library.entity.Book;
 import org.spring.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
+public interface BookRepository extends JpaRepository<Book, Long> {}
