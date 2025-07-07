b81885e3a6be4c8d5ccc7dee2bd2f94c48a1934d
Reethika <itsme.reethu004@gmail.com>
2025-07-07 23:08:47 +0530

    Ex 5

---
+++ Week3/Week3_SpringData_JPA_HandsOn/05_CountryService/build/resources/main/data.sql
@@ -0,0 +1,6 @@
+DELETE FROM country;
+
+INSERT INTO country (co_code, co_name) VALUES ("IN", "India");
+INSERT INTO country (co_code, co_name) VALUES ("US", "United States");
+INSERT INTO country (co_code, co_name) VALUES ("FI", "Finland");
+INSERT INTO country (co_code, co_name) VALUES ("IS", "Iceland");
