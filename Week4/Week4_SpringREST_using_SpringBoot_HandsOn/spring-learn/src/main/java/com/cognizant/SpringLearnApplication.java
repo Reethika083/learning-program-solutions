39057a61d23242d8b0cd3562e2f175c64ce5b714
Reethika <itsme.reethu004@gmail.com>
2025-07-13 22:42:46 +0530

    Completed week 4 mandatory ex 3

--- Week4/Week4_SpringREST_using_SpringBoot_HandsOn/spring-learn/src/main/java/com/cognizant/SpringLearnApplication.java
+++ Week4/Week4_SpringREST_using_SpringBoot_HandsOn/spring-learn/src/main/java/com/cognizant/SpringLearnApplication.java
@@ -1,25 +1,31 @@
+package com.cognizant;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@SpringBootApplication(scanBasePackages = "com.cognizant.springlearn")
 public class SpringLearnApplication {
 
-    public static void main(String[] args) {
-        displayDate();
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		displayDate();
+	}
 
-    public static void displayDate() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
-        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+	public static void displayDate() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
 
-        try {
-            Date date = format.parse("31/12/2018");
-            System.out.println("Parsed Date: " + date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			Date date = format.parse("31/12/2018");
+			System.out.println("Parsed Date: " + date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
 }
