f0194de057bfc37dba96ab8c6c029a97db8ae9f4
Reethika <itsme.reethu004@gmail.com>
2025-07-13 22:27:20 +0530

    Completed week 4 mandatory ex 2

--- Week4/Week4_SpringREST_using_SpringBoot_HandsOn/spring-learn/src/main/java/com/cognizant/SpringLearnApplication.java
+++ Week4/Week4_SpringREST_using_SpringBoot_HandsOn/spring-learn/src/main/java/com/cognizant/SpringLearnApplication.java
@@ -1,12 +1,25 @@
-package com.cognizant.spring-learn;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@SpringBootApplication
 public class SpringLearnApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-		System.out.println("SpringLearnApplication is running...");
-	}
+
+    public static void main(String[] args) {
+        displayDate();
+    }
+
+    public static void displayDate() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+        SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+
+        try {
+            Date date = format.parse("31/12/2018");
+            System.out.println("Parsed Date: " + date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
