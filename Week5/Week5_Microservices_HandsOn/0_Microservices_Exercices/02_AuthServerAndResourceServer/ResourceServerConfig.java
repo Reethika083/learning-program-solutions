8eaeb45ed9db78bdb46fa6743c56d29a4a43fc3c
Reethika <itsme.reethu004@gmail.com>
2025-07-19 00:28:31 +0530

    Completed ex 2

--- Week5/Week5_Microservices_HandsOn/0_Microservices_Exercices/02_AuthServerAndResourceServer/ResourceServerConfig.java
+++ Week5/Week5_Microservices_HandsOn/0_Microservices_Exercices/02_AuthServerAndResourceServer/ResourceServerConfig.java
@@ -1,16 +1,22 @@
+package com.reethika.resource;
+
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable() // 🔐 disable CSRF
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .anyRequest().permitAll() // ✅ allow all without login
                 .and()
-                .oauth2ResourceServer()
-                .jwt();
+                .httpBasic().disable() // ❌ no basic auth
+                .formLogin().disable(); // ❌ no form login
     }
 }
