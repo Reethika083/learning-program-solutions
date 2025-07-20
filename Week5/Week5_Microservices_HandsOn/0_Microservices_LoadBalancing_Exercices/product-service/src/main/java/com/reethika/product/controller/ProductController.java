0b882f0ce2590474a3cc5de6c7af87f8b146452e
Reethika <itsme.reethu004@gmail.com>
2025-07-20 10:40:22 +0530

    Completed load balancing exercices

---
+++ Week5/Week5_Microservices_HandsOn/0_Microservices_LoadBalancing_Exercices/product-service/src/main/java/com/reethika/product/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.reethika.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "📦 Product Service - Products List";
+    }
+}
