468e5a9b7014cdae1dea9f95113cd299555b9c91
Reethika <itsme.reethu004@gmail.com>
2025-07-25 22:08:25 +0530

    Completed 6 react hol handson

---
+++ Week6/trainersapp/src/Home.js
@@ -0,0 +1,13 @@
+// src/Home.js
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Cognizant Trainer Portal</h2>
+      <p>Explore the list of amazing trainers and their expertise.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ Week6/trainersapp/src/TrainersMock.js
@@ -0,0 +1,10 @@
+// src/TrainersMock.js
+import Trainer from "./Trainer";
+
+const trainers = [
+  new Trainer(1, "Ankit Sharma", "ankit@example.com", "1234567890", "React", ["JavaScript", "Redux", "Hooks"]),
+  new Trainer(2, "Priya Sinha", "priya@example.com", "9876543210", "Java", ["Spring Boot", "Hibernate"]),
+  new Trainer(3, "Ravi Kumar", "ravi@example.com", "9998887776", "Python", ["Django", "Flask", "Pandas"]),
+];
+
+export default trainers;
