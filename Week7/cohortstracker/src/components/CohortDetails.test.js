38ed936727328061696e25e4e140bc96e25e2dda
Reethika <itsme.reethu004@gmail.com>
2025-08-03 17:06:55 +0530

    Completed all the exercices of react

---
+++ Week7/cohortstracker/src/components/CohortDetails.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import CohortDetails from './CohortDetails';
+import { CohortData } from './Cohort';
+
+test('renders a cohort item', () => {
+  render(<CohortDetails cohorts={CohortData} />);
+  const cohortElement = screen.getByText(/cohort/i); // Update this if your cohort names are different
+  expect(cohortElement).toBeInTheDocument();
+});
